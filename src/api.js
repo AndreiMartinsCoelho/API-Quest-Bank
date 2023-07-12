3fe02ea0b333b317962365934cea430e2e11f220
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-07-12 15:57:50 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const {
+    BASE_URL,
+    DB_HOST,
+    DB_USER,
+    DB_USER_PASS,
+    DB_DATEBASE
+} = require('./config');
+
+const router = require('./router/route');
+app.use(router);
+
+const userRoute = require("./router/user");
+app.use('/user', userRoute);
+
+const login = require('./router/user');
+app.use('/login', login);
+
+module.exports = app;
