b43b32fec74331f3a0cd6c4997029eb8ed694d4c
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-07-13 17:10:09 -0300

    resetSenha ajustes

--- src/router/resetSenha.js
+++ src/router/resetSenha.js
@@ -2,32 +2,15 @@ const express = require("express");
 const router = express.Router();
 const resetController = require("../controller/resetController");
 
-router.post("/", async (req, res) => {
-  const data = req.body;
-  const result = await resetController.changePassword(data);
-
-  if (result.success) {
-    res.status(200).json(result);
-  } else {
-    res.status(401).json(result);
-  }
+router.put("/", async (req, res, next) => {
+  const result = await resetController.changePassword(req.body);
+  res.status(200).send(result);
 });
 
-router.post("/reset", async (req, res) => {
-  const data = req.body;
-  const result = await resetController.changePassword(data);
-
-  if (result.success) {
-    res.status(200).json({
-      auth: true,
-      user: result.user,
-    });
-  } else {
-    res.status(401).json({
-      auth: false,
-      message: result.message,
-    });
-  }
-});
+router.put("/reset", async (req, res, next) => {
+    const result = await resetController.changePassword(req.body);
+    res.status(200).send(result);
+  });
+  
 
 module.exports = router;
