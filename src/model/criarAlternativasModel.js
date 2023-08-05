e8ee349bf148dc6e33b7c1a2d4834a8c199dd1f2
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-08-05 12:58:04 -0300

    criar alternativas ajustes

--- src/model/criarAlternativasModel.js
+++ src/model/criarAlternativasModel.js
@@ -67,6 +67,32 @@ const obterIdQuestaoPorEnunciado = async (enunciadoQuestao) => {
     }
 };
 
+//Função para obter todas as alternativas
+const get = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT a.id_alternativa, a.correta, a.questao_id_questao, a.enunciado
+            FROM infocimol.alternativa a
+            JOIN questao q ON a.questao_id_questao = q.id_questao;`,
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    const alternativas = results.map((alternativa) => ({
+                        id_alternativa: alternativa.id_alternativa,
+                        correta: alternativa.correta,
+                        questao: {
+                            id_questao: alternativa.questao_id_questao,
+                        },
+                        enunciado: alternativa.enunciado
+                    }));
+                    resolve(alternativas);
+                }
+            }
+        );
+    });
+}
+
 // Função para obter o id da alternativa
 const obterNovoIdAlternativa = async () => {
     try {
@@ -90,4 +116,4 @@ const obterNovoIdAlternativa = async () => {
     }
 }
 
-module.exports = { criarAlternativa, obterIdQuestaoPorEnunciado, obterNovoIdAlternativa };
+module.exports = { criarAlternativa, obterIdQuestaoPorEnunciado, obterNovoIdAlternativa, get };
