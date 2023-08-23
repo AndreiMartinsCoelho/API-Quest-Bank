ad6470b75110643a56207014a4a944721c2dc3c7
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-08-23 17:04:45 -0300

    ajustes cors

--- src/config.js
+++ src/config.js
@@ -3,5 +3,19 @@ module.exports = {
     DB_HOST:'mysql.infocimol.com.br',
     DB_USER:'root',
     DB_USER_PASS:'c1i2m3o4l5',
-    DB_DATABASE:'infocimol'
-}
+    DB_DATABASE:'infocimol',
+
+    async headers(){
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              { key: 'Access-Control-Allow-Origin', value: '*' },
+              { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, PATCH, DELETE' },
+              { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With,content-type' },
+              { key: 'Access-Control-Allow-Credentials', value: true }
+            ]  ,          
+          },
+        ];
+    },
+};
