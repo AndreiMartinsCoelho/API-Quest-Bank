845044c694f1948db9106c56b3fc497908d77178
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-08-23 19:33:36 -0300

    att acordar

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const app = require('./src/api');
-app.use (function (req, res, next) {
-    next();
+const app = require("./src/api");
+
+let apiIsAwake = false; //variavel para verificar se a API está acordada
+
+app.use(function (req, res, next) {
+  //condigo para acordar a API caso ela esteja dormindo
+
+  if (!apiIsAwake) {
+    console.log("API estava dormindo. Acordando agora...");
+    apiIsAwake = true;
+  }
+  console.log("API está acordada!");
+  // Continue com o processamento normal da requisição
+  next();
 });
 
 // Define a porta do servidor e o inicia
