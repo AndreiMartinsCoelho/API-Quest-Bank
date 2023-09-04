b137176d2c39d60d5750e86b167419276140e415
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-09-04 14:12:35 -0300

    :bulb:HEADERS em construção

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ headers = () =>{
         { key: 'Access-Control-Allow-Origin', value: '*' },
         { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, PATCH, DELETE' },
         { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With,content-type' },
-        { key: 'Access-Control-Allow-Credentials', value: true },
-        { key: 'x-access-token', value: 'true'},
-        { key: 'perfil', value: 'admin'}
+        { key: 'Access-Control-Allow-Credentials', value: true }
+        // { key: 'x-access-token', value: 'true'},
+        // { key: 'perfil', value: 'admin'}
       ],    
     },
   ];
