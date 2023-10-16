f105214f78733f0e5bbed81d8a371a7c1b74557a
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-10-16 10:42:19 -0300

    :bug: Melhor

--- src/api.js
+++ src/api.js
@@ -20,11 +20,7 @@ exports.headers(async () =>{
         source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, PATCH, DELETE' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With,content-type' },
-          { key: 'Access-Control-Allow-Credentials', value: true },
-          { key: 'x-access-token', value: 'true'},
-          { key: 'perfil', value: 'admin', value: 'professor'}
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, PATCH, DELETE' }
         ],
       },
     ];
