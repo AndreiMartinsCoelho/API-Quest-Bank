ef0190356614c37b04d0c11b7a47592fb0912113
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-11-03 23:33:02 -0300

    :hammer: Melhoria em todos os CODIGOs, exeto nos USERs

--- src/router/Topico.js
+++ src/router/Topico.js
@@ -10,18 +10,18 @@ TopicoRouter.get('/topico/listar', Middleware.verifyToken, TopicoController.get)
 TopicoRouter.get('/topicos/listar', Middleware.verifyToken, TopicoController.getTodosTopicos);
 
 //----Rota para ADICIONAR um TOPICO----
-TopicoRouter.post('/topico/adicionar', /*Middleware.verifyToken,*/ TopicoController.criar);
+TopicoRouter.post('/topico/adicionar', Middleware.verifyToken, TopicoController.criar);
 
 //----Rota para DELETAR um TOPICO----
 TopicoRouter.delete('/topico/deletar/:id', Middleware.verifyToken, TopicoController.excluir);
 
 //----Rota para ATUALIZAR um TOPICO----
-TopicoRouter.put('/topico/atualizar/:id', /*Middleware.verifyToken,*/ TopicoController.editar);
+TopicoRouter.put('/topico/atualizar/:id', Middleware.verifyToken, TopicoController.editar);
 
 //----Rota para LISTAR um TOPICO específico pelo seu ID----
 TopicoRouter.get('/topico/listar/:id', Middleware.verifyToken, TopicoController.listaId);
 
 //----Rota para BUSCAR um TOPICO pelo ENUNCIADO----
-TopicoRouter.get('/topico/buscar/:enunciado', Middleware.verifyToken,  TopicoController.buscar);
+TopicoRouter.get('/topico/buscar/:enunciado', Middleware.verifyToken, TopicoController.buscar);
 
 module.exports = TopicoRouter;
